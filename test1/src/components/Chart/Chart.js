d7d297f8aa40f2897b74b16e64cc0bd5181a2a9e
praguru14 <48213609+praguru14@users.noreply.github.com>
2021-07-19 17:35:58 +0530

    Conditional content

---
+++ test1/src/components/Chart/Chart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ChartBar from './ChartBar'
+import './Chart'
+function Chart(props) {
+    return (
+       <div className="chart">
+           
+       </div>
+    )
+}
+
+export default Chart
